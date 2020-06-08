84b7052fad3f24f000acb066b0558ef668e27f87
Nathan Nguyen <natn2323@gmail.com>
2020-06-07 20:30:24 -0700

    Make results section tabbable.

--- marf/src/app/calculators/dot-product-calculator/dot-product-form/dot-product-form.component.ts
+++ marf/src/app/calculators/dot-product-calculator/dot-product-form/dot-product-form.component.ts
@@ -81,7 +81,7 @@ export class DotProductFormComponent implements OnInit {
     }
     // return an observable with a user-facing error message
     return throwError(errorMessage);
-  };
+  }
 
   onSubmit(): boolean {
     // TODO: Make sure that the submission cannot occur while the vectorLengthOnChange function is being run
@@ -129,13 +129,6 @@ export class DotProductFormComponent implements OnInit {
       return false;
     }
     
-    // console.log(event.key);
-    // const element = document.querySelector('input.counter');
-    // const previousLength = element.style.width || 0;
-    // console.log('width is: ' + previousLength);
-    // console.log('nice: ' + element.style.width);
-    // element.style.width = (element.style.width + previousLength + 1) + 'ch';
-
     return true;
   }
 
--- marf/src/app/calculators/dot-product-calculator/dot-product-results/dot-product-results.component.ts
+++ marf/src/app/calculators/dot-product-calculator/dot-product-results/dot-product-results.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-dot-product-results',
   templateUrl: './dot-product-results.component.html',
   styleUrls: ['./dot-product-results.component.less']
 })
-export class DotProductResultsComponent implements OnInit {
+export class DotProductResultsComponent implements OnInit, OnChanges {
   @Input() result: string;
   @Input() errorMessage: string;
 
   constructor() { }
 
   ngOnInit(): void { }
+
+  ngOnChanges(changes: SimpleChanges): void { }
 
 }
