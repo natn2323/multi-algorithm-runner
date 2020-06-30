89c49160a1030a06331d2d17eb244fb06e072804
Nathan Nguyen <natn2323@gmail.com>
2020-06-29 22:15:23 -0700

    Update styling for website; remove unused code; made accessibility changes for prime number checker.

--- marf/src/app/calculators/find-primes/find-primes.component.ts
+++ marf/src/app/calculators/find-primes/find-primes.component.ts
@@ -1,12 +1,7 @@
-import { Component, OnInit, SimpleChanges, Input } from '@angular/core';
-import { MatFormField } from '@angular/material/form-field';
-import { MatInput } from '@angular/material/input';
-import { MatButton } from '@angular/material/button';
-import { ErrorStateMatcher } from '@angular/material/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { throwError, ObservableInput } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
@@ -14,7 +9,6 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
   animations: [ // Can probably strip the entire "expander" into its own component
     trigger('expandCollapse', [
       state('activated', style({
-        // 'transition-delay': '1s',
         transform: 'translateX(30px) rotate(90deg)'
       })),
       state('*', style({
@@ -32,15 +26,16 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
   styleUrls: ['./find-primes.component.less']
 })
 export class FindPrimesComponent implements OnInit {
+  title: string = 'Prime Number Checker';
   configUrl: string = '/api/findprimes';
-  maxCharacters: number = 100;
+  maxNumberValue: number = 1000000;
 
-  findPrimesForm: FormGroup;
-  inputList: number[] = null;
+  input: string = null;
 
-  isSubmitting: boolean = false; // Want to use this to animate the frontend
-
-  haveResult: boolean = false;
+  isSubmitting: boolean = false;
+  hasValidationError: boolean;
+  validationErrorMessage: string;
+  hasResult: boolean = false;
   primes: number[];
   nonPrimes: number[];
   respMessage: string;
@@ -48,21 +43,36 @@ export class FindPrimesComponent implements OnInit {
 
   constructor(private http: HttpClient) { }
 
-  ngOnInit(): void {
-    this.findPrimesForm = new FormGroup({
-      'inputString': new FormControl(null, 
-        [
-          Validators.required, 
-          Validators.maxLength(this.maxCharacters),
-          Validators.pattern(/^-?([0-9])+(,-?[0-9]+)*$/)
-        ])
-    });
+  ngOnInit(): void { }
+
+  private parseInput(input: string): number[] {
+    if (!input) {
+      this.hasValidationError = true;
+      this.validationErrorMessage = 'Input is empty. Please input a number.';
+      return null;
+    }
+    else {
+      const inputList: number[] = input.split(',').map(element => parseInt(element));
+      let i: number;
+      
+      for (i = 0; i < inputList.length; i++) {
+        if (Number.isNaN(inputList[i])) {
+          this.hasValidationError = true;
+          this.validationErrorMessage = 'Input is invalid. Only integers are accepted.';
+          return null;
+        }
+        else if (inputList[i] > this.maxNumberValue) {
+          this.hasValidationError = true;
+          this.validationErrorMessage = 'Max number is 1,000,000. Please use smaller numbers.';
+          return null;
+        }
+      }
+      
+      this.hasValidationError = false;
+      return inputList;
+    }
   }
 
-  get inputString() { return this.findPrimesForm.get('inputString'); }
-
-  // TODO: Show hints on maximum number value, possibly maximum count of numbers to include: https://material.angular.io/components/form-field/overview#hint-labels
-  
   // This function was taken from an online source.
   private handleError(error: HttpErrorResponse): ObservableInput<any> {
     let errorMessage: string = '';
@@ -78,30 +88,35 @@ export class FindPrimesComponent implements OnInit {
     // return an observable with a user-facing error message
     return throwError(errorMessage);
   }
-  
+
   onSubmit(): boolean {
-    if (this.inputString.valid) {
-      this.haveResult = false;
+    // Reset the values so that they are not detected by screen readers when focusing on the "Results" chip
+    this.hasResult = false;
+    this.primes = null;
+    this.nonPrimes = null;
+    this.respMessage = null;
+    this.respHasError = false;
+
+    const parsedInput = this.parseInput(this.input);
+
+    if (parsedInput !== null) {
       this.isSubmitting = true;
-
-      // TODO: Make sure that the submission cannot occur while waiting on response
-      this.inputList = this.inputString.value.split(',').map(element => parseInt(element));
-      
+  
       const data = {
-        list: this.inputList
+        list: parsedInput
       };
-
+  
       const requestHeaders = new HttpHeaders({
         'Content-Type': 'application/json'
       });
-
+  
       this.http.post(this.configUrl, data, { headers: requestHeaders, observe: 'response', responseType: 'json'})
         .pipe(catchError(this.handleError))
         .subscribe(
           res => {
-            this.haveResult = true;
+            this.hasResult = true;
             this.isSubmitting = false;
-
+  
             if (res.body.data && 
               (res.body.data['primes'] || 
               res.body.data['non_primes'] ||
@@ -109,7 +124,7 @@ export class FindPrimesComponent implements OnInit {
                 // Parse the primes and non-primes lists
                 this.primes = res.body.data['primes'].length > 0 ? res.body.data['primes'] : null;
                 this.nonPrimes = res.body.data['non_primes'].length > 0 ? res.body.data['non_primes'] : null;
-
+  
                 this.respMessage = res.body.data['message'];
             }
             else if (res.body.data && res.body.data['has_error'] && res.body.data['message']) {
@@ -118,16 +133,14 @@ export class FindPrimesComponent implements OnInit {
             }
           },
           err => {
-            this.haveResult = true;
+            this.hasResult = true;
             this.isSubmitting = false;
             this.respHasError = true;
             this.respMessage = err;
         });
-
-      return false;
     }
-  }
 
-  ngOnChanges(changes: SimpleChanges): void { }
+    return false;
+  }
 
 }
